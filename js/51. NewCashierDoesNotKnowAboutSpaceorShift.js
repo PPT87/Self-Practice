73aefdd7aa520579d865767ec32d86b604b5cf3e
PeterT <tranpp87@gmail.com>
2022-01-09 00:01:18 -0500

    update challenge description

--- js/51. NewCashierDoesNotKnowAboutSpaceorShift.js
+++ js/51. NewCashierDoesNotKnowAboutSpaceorShift.js
@@ -1,5 +1,23 @@
 /*****************NOT COMPLETED!!************/
 
+// Some new cashiers started to work at your restaurant.
+
+// They are good at taking orders, but they don't know how to capitalize words, or use a space bar!
+
+// All the orders they create look something like this:
+
+// "milkshakepizzachickenfriescokeburgerpizzasandwichmilkshakepizza"
+
+// The kitchen staff are threatening to quit, because of how difficult it is to read the orders.
+
+// Their preference is to get the orders as a nice clean string with spaces and capitals like so:
+
+// "Burger Fries Chicken Pizza Pizza Pizza Sandwich Milkshake Milkshake Coke"
+
+// The kitchen staff expect the items to be in the same order as they appear in the menu.
+
+// The menu items are fairly simple, there is no overlap in the names of the items:
+
 // 1. Burger
 // 2. Fries
 // 3. Chicken
@@ -8,12 +26,6 @@
 // 6. Onionrings
 // 7. Milkshake
 // 8. Coke
-
-//Their preference is to get the orders as a nice clean string with spaces and capitals like so:
-
-// "Burger Fries Chicken Pizza Pizza Pizza Sandwich Milkshake Milkshake Coke"
-
-input = "milkshakepizzachickenfriescokeburgerpizzasandwichmilkshakepizza"
 
 function getOrder(input){
   // console.log(input.includes('pizza', 'milk', 'burger', 'chicken', 'onionrings', 'milkshake', 'coke', 'sandwich'))
