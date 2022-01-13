cc9e0389f2459f78d00871e0de084a4f73889228
PeterT <tranpp87@gmail.com>
2022-01-13 11:34:48 -0500

    add additional solution to problem 55

--- js/55. findTheUniqueNumber.js
+++ js/55. findTheUniqueNumber.js
@@ -10,21 +10,46 @@
     // (findUniq([ 1, 1, 2, 1, 1 ]), 2);
     // (findUniq([ 3, 10, 3, 3, 3 ]), 10);
 
-    function findUniq(arr){
-      let uniq = {}
+
+    /***************************************************************/
+    //Solution 1
+
+    // function findUniq(arr){
+    //   let uniq = {}
+    //   for (let i = 0; i < arr.length; i++){
+    //     let num = arr[i]
+    //     if (uniq[num]){
+    //       uniq[num]++
+    //     } else {
+    //       uniq[num] = 1
+    //     }
+    //   }
+    //   for (let i in uniq){
+    //     if (uniq[i] === 1){
+    //       return parseFloat(i)
+    //     }
+    //   }
+    // }
+
+    
+    
+    /***************************************************************/
+    //Solution 2
+    
+    let findUniq = (arr) =>{
+      let notUniq;
       for (let i = 0; i < arr.length; i++){
-        let num = arr[i]
-        if (uniq[num]){
-          uniq[num]++
-        } else {
-          uniq[num] = 1
+        if (arr[i] === arr[i+1] || arr[i] === arr[i+2]){
+          notUniq = arr[i]
+          break
         }
       }
-      for (let i in uniq){
-        if (uniq[i] === 1){
-          return parseFloat(i)
+      for (let j = 0; j < arr.length; j++){
+        if (arr[j] !== notUniq){
+          return arr[j]
         }
       }
     }
-
+    
+    
     console.log(findUniq([0, 0, 0.55, 0, 0]));
