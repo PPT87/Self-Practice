92251ff79943ba0c16a517dece8bf06a55aceb01
PeterT <tranpp87@gmail.com>
2022-02-18 10:44:05 -0500

    solve challenge 89

---
+++ js/89. seekAndDestroy.js
@@ -0,0 +1,11 @@
+//Remove from the array whatever is in the following arguments. return the leftover values in an array
+// ex. seekAndDestroy([2,3,4,6,6, 'hello'], 2,6) == [3,4,'hello']
+
+
+const seekAndDestroy = (arr, ...rest) => {
+  return arr.filter(val => !rest.includes(val))
+}
+
+
+
+console.log(seekAndDestroy([2,3,4,6,6, 'hello'], 2,6)) 
