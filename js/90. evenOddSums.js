cdd72d195a335c191923c9a1c3e99ffca9d8eded
PeterT <tranpp87@gmail.com>
2022-02-19 14:10:24 -0500

    solve challenge 91

--- js/90. evenOddSums.js
+++ js/90. evenOddSums.js
@@ -1,18 +1,31 @@
 // Take in an array and return an array of the sums of even and odd numbers
 // ex. evenOddSums([50,60,60,45,71]) == [170,116]
 
+
+//Solution 1
+// const evenOddSums = (arr) => {
+//   let oddSumArr = []
+//   let evenSumArr = []
+
+//   arr.forEach(num => {
+//     num % 2 === 0 ? evenSumArr.push(num) : oddSumArr.push(num)
+//   });
+
+//   oddSumArr = oddSumArr.reduce((acc, curr) => acc + curr)
+//   evenSumArr = evenSumArr.reduce((acc, curr) => acc + curr)
+
+//   return [oddSumArr, evenSumArr]
+// }
+
+
+//Solution 2
 const evenOddSums = (arr) => {
-  let oddSumArr = []
-  let evenSumArr = []
+  let oddSum = 0
+  let evenSum = 0
 
-  arr.forEach(num => {
-    num % 2 === 0 ? evenSumArr.push(num) : oddSumArr.push(num)
-  });
+  arr.forEach(num => (num % 2 === 0 ? (evenSum += num) : (oddSum += num)))
 
-  oddSumArr = oddSumArr.reduce((acc, curr) => acc + curr)
-  evenSumArr = evenSumArr.reduce((acc, curr) => acc + curr)
-
-  return [oddSumArr, evenSumArr]
+  return([evenSum, oddSum])
 }
 
 
