48037fedefacc82a64a4b1cd96c2e88282228020
PeterT <tranpp87@gmail.com>
2022-04-15 10:28:04 -0400

    solve challenge 112

--- js/111. largestPerimeterTriangle.js
+++ js/111. largestPerimeterTriangle.js
@@ -1,7 +1,6 @@
 // Given an integer array nums, return the largest perimeter of a triangle with a non-zero area, formed from three of these lengths. If it is impossible to form any triangle of a non-zero area, return 0.
 
 
-
 // Example 1:
 
 // Input: nums = [2,1,2]
